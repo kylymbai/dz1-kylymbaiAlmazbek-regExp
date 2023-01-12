9f0239cc034d3ace95fd62493b4f96dcb47dd6f6
Kylymbai <kylymbaialmazbekuulu@gmail.com>
2023-01-13 01:22:07 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+//Задание - 1
+
+// const inputNum = document.querySelector('#inputNum')
+// const btn = document.querySelector('#checkBtn')
+// const result = document.querySelector('#numResult')
+
+// const regExp = /^\+7 \d{3} \d{7}$/
+
+// const checkNumber = () => {
+//     if(regExp.test(inputNum.value)) {
+//         result.innerText = 'True:)'
+//         result.style.color = 'green'
+//     }else {
+//         result.innerText = 'False:('
+//         result.style.color = 'red'
+//     }
+// }
+
+// btn.addEventListener('click', checkNumber)
+
+//задание - 2
+
+// const bigBox = document.querySelector('.bigBox')
+// const smallBox = document.querySelector('.smallBox')
+
+// let pos = 0
+
+// const moveBox = () => {
+//   pos++
+//   smallBox.style.left = `${pos}px`
+//   if(pos < 450){
+//     moveBox()
+//   }
+  
+// }
+
+// moveBox()
